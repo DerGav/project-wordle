b06943342c38398af9462394cfbd8c0baf9c559a
gavingabriel <gavin.gabriel@susiandjames.com>
2025-03-17 10:06:00 +0100

    use htmlFor

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -15,7 +15,7 @@ function GuessInput() {
   };
 
   return <form class="guess-input-wrapper" onSubmit={handleSubmit}>
-    <label for="guess-input">Enter guess:</label>
+    <label htmlFor="guess-input">Enter guess:</label>
     <input 
       id="guess-input"
       type="text" 
